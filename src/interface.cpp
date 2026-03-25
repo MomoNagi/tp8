442ae786c569adb330a82ef76d360038e7fcc294
MomoNagi <lemanhhungtitdepzai@gmail.com>
2026-03-24 23:27:30 -0400

    update interface.cpp docs

--- src/interface.cpp
+++ src/interface.cpp
@@ -26,5 +26,37 @@ py::array_t<double> interface_cum_médiane(py::array_t<double> numpy_array_de_do
   }
 
 PYBIND11_MODULE(cum_ext_module, m, py::mod_gil_not_used()) {
-    m.def("cum_médiane", &interface_cum_médiane, "Calcule la médiane cumulative!");
+    m.doc() = "Module d'extension C++ pour calculs statistiques cumulatifs.";
+
+    m.def("cum_médiane", &interface_cum_médiane, R"pbdoc(
+    Calcule la médiane cumulative d'un tableau NumPy.
+
+    L'algorithme utilise un ensemble trié pour maintenir les valeurs et 
+    calculer la médiane à chaque nouvelle insertion.
+
+    Parameters
+    ----------
+    numpy_array_de_doubles : numpy.ndarray
+        Un tableau 1D de type float64 contenant les données d'entrée dont les valeurs du tableau doivent être uniques.
+
+    Returns
+    -------
+    numpy.ndarray
+        Un tableau de type float64 contenant la médiane calculée 
+        à chaque étape.
+
+    Raises
+    ------
+    RuntimeError
+        Si le tableau contient des données répétées (doublons).
+
+    Examples
+    --------
+    >>> import cum_ext_module
+    >>> import numpy as np
+    >>> data = np.array([2.0, 3.0, 0.0, 1.0, 5.0])
+    >>> cum_ext_module.cum_médiane(data)
+    array([2.0, 2.5, 2.0, 1.5, 2.0])
+    )pbdoc");
 }
+
